6917ffb48e9d1c0f3deaa9ccfb1c27a8b49fa57a
lullebulle1 <lvmd04@gmail.com>
2021-12-15 16:39:55 +0100

    final

--- mian.py
+++ mian.py
@@ -1,16 +1,26 @@
+import time
+import random
+import os
+def quit():
+    os.exit
+def clearconsol():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):
+        command='cls'
+    os.system(command)
 
 
 önskelista=input("""
 many:
 1)skapa önskelista
 2)läs upp önskelista
+3)naughty list 
 """)
 
 if "skapa önskelista" in önskelista:
     x=input("vad heter filen: ")
     with open(x,"w", encoding="utf8" ) as x:
         print("mata in vad dina barn heter: ")
-
         barn=input("barnets namn: ")
         x.write(barn+"\n")
         for pryl in range (10):
@@ -20,4 +30,31 @@ if "skapa önskelista" in önskelista:
 if "läs upp önskelista" in önskelista:
     y=input("vad heter filen du vill läsa up: ")
     with open(y, "r", encoding="utf8") as f:
-        f.readlines(y)
+        time.sleep(2)
+        print(f.read())
+
+if "naughty list" in önskelista:
+    list=input(f"is this the file you want to use: naughtylist.txt? ")
+    if "yes" in list:
+        print("this is the kid on the naughty list: ")
+        with open("naughtylist.txt", "r", encoding="utf8") as naughtylist:
+            print(naughtylist.read())
+    else:
+        quit()
+add=input("do you want to adda to the naughty list?")
+if "yes" in add:
+    lol=" "
+    with open("naughtylist.txt","a", encoding="utf8" ) as append:
+        while True:
+            lol=input("kids name, press space to stop: ")
+            if " " in lol:
+                print(naughtylist.read)
+                break
+            else:
+                append.write(lol+"\n")
+                pass
+    time.sleep(1)
+    with open("naughtylist.txt", "r", encoding="utf8") as naughtylist:
+            print(naughtylist.read() + lol)
+
+        
